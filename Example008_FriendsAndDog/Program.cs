e4113b4c73355e4f0e7b74f0c18c3aeb36571d1b
ThinksSo <stpt.mail@yandex.ru>
2022-09-15 17:59:54 +0300

    add Example 008 Friends and dog

---
+++ Example008_FriendsAndDog/Program.cs
@@ -0,0 +1,33 @@
+﻿int firstFriendSpeed = 4;
+int secondFriendSpeed = 5;
+int dogSpeed = 20;
+double distance = 100;
+int distanceLimith = 10;
+int dogCounter = 0;
+int dogDirection = 1;
+double timeToMeet = 0;
+double speed = 0;
+
+while (distance > distanceLimith)
+{
+  if (dogDirection == 1)
+  {
+    speed = firstFriendSpeed + dogSpeed;
+    dogDirection = 0;
+    // Console.WriteLine("Change direction to second Friend");
+  }
+  else
+  {
+    speed = secondFriendSpeed + dogSpeed;
+    dogDirection = 1;
+    // Console.WriteLine("Change direction to first Friend");
+  }
+
+    timeToMeet = distance / speed;
+    distance = distance - timeToMeet * (firstFriendSpeed + secondFriendSpeed);
+    dogCounter = dogCounter + 1;
+    Console.Write("Distance ");
+    Console.WriteLine(distance);
+}
+Console.Write("Count = ");
+Console.WriteLine(dogCounter);
