c54c34f32fffdf48ac6e7b8c7305e9c4ef6353d0
ThinksSo <stpt.mail@yandex.ru>
2022-09-15 22:11:59 +0300

    add Seminar 004 Function

---
+++ Seminar004_Function/Program.cs
@@ -0,0 +1,18 @@
+﻿//ФУНКЦИИ
+Console.Clear();
+Console.WriteLine();
+Console.WriteLine("ФУНКЦИИ");
+
+
+//Функция возведения числа в квадрат
+int Power(int x)
+{
+    return x * x;
+}
+
+Console.Write("Input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Power(number));
+Console.WriteLine();
+
