5bc662fd7683e1e4cdec9012cb91bd50f7f6db3f
ThinksSo <stpt.mail@yandex.ru>
2022-09-15 21:58:30 +0300

    add Seminar 003 Task2 Week day

---
+++ Seminar003_Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Task3: Определения для недели
+Console.WriteLine();
+
+Console.Write("Input number ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1) {Console.WriteLine("Today is Mo");}
+else if (day == 2) {Console.WriteLine("Today is Tu");}
+else if (day == 3) {Console.WriteLine("Today is We");}
+else if (day == 4) {Console.WriteLine("Today is Th");}
+else if (day == 5) {Console.WriteLine("Today is Fr");}
+else if (day == 6) {Console.WriteLine("Today is Sa");}
+else if (day == 7) {Console.WriteLine("Today is Su");}
+else {Console.WriteLine("Incorrect input");}
+
+Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
