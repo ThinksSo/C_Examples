00f9af4b3bde84f331b9da07f44c033d555ae0b4
ThinksSo <stpt.mail@yandex.ru>
2022-09-15 21:35:29 +0300

    add Seminar 002 Task1

---
+++ Seminar002_Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//ПTask: роверка числа на результат возведения в квадрат
+
+Console.Write("Input number 1 (square) ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2 * number2)
+{
+    Console.WriteLine($"Yea, {number1} is square of {number2}");
+}
+else
+{
+    Console.WriteLine("No");
+}
+Console.WriteLine();
