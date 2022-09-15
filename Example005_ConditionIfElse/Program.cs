3990f97a9f5bf3067fcf1a1dfbca76bb153cb21c
ThinksSo <stpt.mail@yandex.ru>
2022-09-15 16:12:45 +0300

    add Example005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input user name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "papa")
+{
+    Console.Write("Welome ");
+    Console.WriteLine(username.ToUpper());
+}
+else
+{
+    Console.Write("Hi ");
+    Console.WriteLine(username);
+}
