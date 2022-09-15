38494855cc649296f9d4df1c03bb201390eca163
ThinksSo <stpt.mail@yandex.ru>
2022-09-15 17:20:49 +0300

    add Example 007 Magic (cycle while)

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+
+int x= xa, y = xb;
+int count = 0;
+
+while(count < 10000)
+{
+    int numR = new Random().Next(0, 3);
+    if (numR == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (numR == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (numR == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count ++;
+}
