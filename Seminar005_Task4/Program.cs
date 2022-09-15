4f3a47ff378627ed925f1b2c7805828049d7db13
ThinksSo <stpt.mail@yandex.ru>
2022-09-15 22:34:44 +0300

    add Seminar Task5

---
+++ Seminar005_Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task4: Вывод чисел в промежутке
+Console.WriteLine();
+
+Console.Write("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+int num2 = -num;
+
+while (num2 <= num)
+{
+    Console.Write(num2 + ", ");
+    num2 = num2 + 1;
+}
+
+Console.WriteLine();
