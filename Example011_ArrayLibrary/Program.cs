5ed707b7eef26603303f6a3dd09e29eb14fc9b02
ThinksSo <stpt.mail@yandex.ru>
2022-09-17 18:42:15 +0300

    Example011_ArrayLibrary update for any array

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -20,9 +20,37 @@ void PrintArray(int[] lib)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
 Console.WriteLine();
 
+Console.Write("Enter number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int pos = IndexOf(array, x);
+
+if (pos == -1) 
+{
+    Console.WriteLine("No such element");
+}
+else Console.WriteLine(pos);
+
+Console.WriteLine();
