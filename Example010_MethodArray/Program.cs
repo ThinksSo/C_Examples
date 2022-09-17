9589474b30f067b0e7ff3c881e79ce5195ca04c5
ThinksSo <stpt.mail@yandex.ru>
2022-09-17 17:01:51 +0300

    add Example010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 12, 23, 54, 2, 99, 75, 40, 34 };
+
+int n = array.Length;
+int find = 75;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;    // index = index +1;
+}
