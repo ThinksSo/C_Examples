8687eb6a85fff04cd5aebf45b493455bedd277b0
ThinksSo <stpt.mail@yandex.ru>
2022-09-17 17:30:28 +0300

    add Example011 Array Library

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1,100);
+        index++;
+    }
+}
+
+void PrintArray(int[] lib)
+{
+    int count = lib.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{lib[position]} ");
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
