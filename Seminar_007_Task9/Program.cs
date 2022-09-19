d6f0a8af1436f04f614c913cd90335d71dbbf566
ThinksSo <stpt.mail@yandex.ru>
2022-09-19 21:38:08 +0300

    Seminar 007 Task9

---
+++ Seminar_007_Task9/Program.cs
@@ -0,0 +1,25 @@
+﻿void Task9()
+{
+    // Вывести случайное число из отрезка [10, 99]
+    // и показать наибольшую цифру из числа
+
+    Random random = new Random(); 
+    // создание объекта Random типа random
+    int num = random.Next(10,100);
+    Console.WriteLine(num);
+
+    int tens = num / 10;
+    int ones = num % 10;
+
+    Console.WriteLine(tens);
+    Console.WriteLine(ones);
+
+    if (tens == ones) 
+    {Console.WriteLine($"a = b = {tens} \n");}
+    else if (tens > ones) 
+    {Console.WriteLine($"amax = {tens} \n");}
+    else 
+    {Console.WriteLine($"max = {ones} \n");}
+}
+
+Task9();
