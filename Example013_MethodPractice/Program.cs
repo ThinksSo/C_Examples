a3b66e6c2e7976ccf89e2620ffbdb5cf4b23cfb3
ThinksSo <stpt.mail@yandex.ru>
2022-09-24 21:01:43 +0300

    Array ordering (упорядочивание массива)

--- Example013_MethodPractice/Program.cs
+++ Example013_MethodPractice/Program.cs
@@ -46,4 +46,47 @@ string Replace(string text, char oldValue, char newValue)
 }
 
 string newText = Replace(text, ' ', '|');
+
 Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
+
+
+
+// Array ordering (упорядочивание массива)
+int[] arr = {1, 3, 8, 4, 2, 9, 7, 3, 0};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+
+SelectionSort(arr);
+PrintArray(arr);
