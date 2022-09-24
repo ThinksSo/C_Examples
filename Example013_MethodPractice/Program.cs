f0ea1a6c7f32b74ed7c748fbff768eca8fdc32e8
ThinksSo <stpt.mail@yandex.ru>
2022-09-24 20:32:01 +0300

    Correction text (замены в тексте)

--- Example013_MethodPractice/Program.cs
+++ Example013_MethodPractice/Program.cs
@@ -25,4 +25,25 @@ for (int i = 2; i < 10; i++)
 
 // Replaces in text (Замены в тексте)
 
+string text = "Три вещи может сделать женщина для русского писателя. " 
+            + "Она может кормить его. "
+            + "Она может искренне поверить в его гениальность. "
+            + "И наконец, женщина может оставить его в покое. "
+            + "Кстати, третье не исключает второго и первого.";
 
+string Replace(string text, char oldValue, char newValue)
+{
+    string result  = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
