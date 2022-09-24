511ccfd458cc4b5028049302dafdfe857ea1e74f
ThinksSo <stpt.mail@yandex.ru>
2022-09-24 19:04:44 +0300

    add Example013_MethodPractice, cycle for, multiplication

---
+++ Example013_MethodPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿//Cycle "for" (цикл "for")
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(5, "bla...");
+Console.WriteLine(res);
+
+
+// Multiplication table (Таблица умножениея)
+for (int i = 2; i < 10; i++)
+{
+    for (int j = 2; j < 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Replaces in text (Замены в тексте)
+
+
