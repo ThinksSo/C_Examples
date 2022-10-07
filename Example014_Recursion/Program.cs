cbb20322930e54f1fc2f81d2e91a41e700337d07
ThinksSo <stpt.mail@yandex.ru>
2022-10-07 13:22:00 +0300

    add Seminar012_DoubleArray

---
+++ Example014_Recursion/Program.cs
@@ -0,0 +1,137 @@
+﻿//ДВУХМЕРНЫЕ МАССИВЫ
+
+void Table1()
+{
+    string[,] table = new string[2, 5];
+    //String.Empty
+    // table [0,0]  table [0,1] table [0,2] table [0,4]
+    // table[1,0]    table[1,1] table[1,2] table[1,4] 
+    table[1, 2] = "text";
+
+    for (int rws = 0; rws < 2; rws++)
+    {
+        for (int columns = 0; columns < 5; columns++)
+        {
+            Console.WriteLine($" - {table[rws, columns]} - ");
+        }
+    }
+}
+
+// Заполнение массива
+void FillArray2X(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Печать массива
+void PrintArray2X(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+// Table1();
+
+int[,] matrix = new int[3, 4];
+FillArray2X(matrix);
+PrintArray2X(matrix);
+
+
+// RECURSION - РЕКУРСИЯ
+
+
+int[,] pic = new int[,]
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+    {0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 },
+    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    {0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 },
+    {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+
+// Print image - печать картинки
+void PrintImag(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            // Console.Write($"{image[i, j]} ");
+            if(image[i,j] == 0) Console.Write(" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Filling image - заполнение/закраска картинки
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+
+PrintImag(pic);
+FillImage(5, 5);
+PrintImag(pic);
+
+
+// Factorial - Факториал
+double Factorial(int n)
+{
+    // 1! = 1; 0! = 1;  3! = 1 * 2 * 3 = 6
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 5; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+Console.WriteLine();
+
+
+// Fibonacci - Числа Фибоначчи
+// 1123581321 -> f(1) = 1, f(2) = 1, f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
---
+++ Seminar012_DoubleArray/Program.cs
@@ -0,0 +1,221 @@
+﻿
+// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+void Task46()
+{
+    Console.WriteLine("\n \t Task 46: 2x Array Random");
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+
+    int[,] numbers = new int[rows, columns];
+    Console.WriteLine($"Array size: {rows}, {columns}");
+
+    FillArray(numbers);
+    PrintArray(numbers);
+
+    Console.WriteLine();
+}
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(-100, 100);
+        }
+
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{numbers[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+/* Задача 48: Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: A = i+j. 
+Выведите полученный массив на экран. m = 3, n = 4. 
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+void Task48()
+{
+    Console.WriteLine("\n \t Task 48: 2x Array i+j");
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+
+    int[,] numbers = new int[rows, columns];
+    Console.WriteLine($"{rows}, {columns}");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = i + j;
+        }
+
+    }
+    PrintArray(numbers);
+    Console.WriteLine();
+}
+
+
+/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты. 
+*/
+void Task49()
+{
+    Console.WriteLine("\n \t Task 49: 2x Array Square elements");
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+    Console.WriteLine($"Array size: {rows}, {columns}");
+    int sum = 0;
+
+    int[,] numbers = new int[rows, columns];
+    FillArray(numbers);
+    PrintArray(numbers);
+
+    for (int i = 0; i < rows; i+=2)
+    {
+        for (int j = 0; j < columns; j+=2)
+        {
+            {
+                numbers[i, j] = numbers[i, j] * numbers[i, j];
+            }
+        }
+    }
+    PrintArray(numbers);
+    Console.WriteLine();
+}
+
+
+/* Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив: 1 4 72
+5 9 23
+8 4 24
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+void Task51()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = rows;
+    int sum = 0;
+    Console.WriteLine($"Array size: {rows}, {columns}");
+
+    int[,] numbers = new int[rows, columns];
+    FillArray(numbers);
+    PrintArray(numbers);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (i == j)
+            {
+                sum = sum + numbers[i, j];
+            }
+        }
+    }
+    Console.WriteLine();
+}
+
+
+/*
+Дан двумерный массив из двух строк и двадцати двух столбцов. В его первой строке записано количество мячей, забитых футбольной командой в той 
+или иной игре, во второй — количество пропущенных мячей в этой же игре. 
+а) Для  каждой  проведенной  игры  напечатать  словесный  результат:  "выигрыш", "ничья" или "проигрыш".
+
+а) Для  каждой  проведенной  игры  напечатать  словесный  результат:  "выигрыш", "ничья" или "проигрыш". 
+б) Определить количество выигрышей данной команды. 
+в) Определить  количество  выигрышей  и  количество  проигрышей  данной команды. 
+г) Определить количество выигрышей, количество ничьих и количество проигрышей данной команды. 
+д) Определить,  в  скольких  играх  разность  забитых  и  пропущенных  мячей была большей или равной трем. 
+е) Определить общее число очков, набранных командой (за выигрыш даются 3 очка, за ничью — 1, за проигрыш — 0).
+*/
+void TaskArayGame()
+{
+    Console.WriteLine("\n \t Task: 2xArray: Win - Lose");
+    Random random = new Random();
+    int rows = 2;
+    int columns = 22;
+    Console.WriteLine($"{rows}, {columns}");
+    int[,] numbers = new int[rows, columns];
+
+    FillArrayCustom(numbers, 0, 10);
+    PrintArray(numbers);
+    int win = 0, lose = 0, draw = 0;
+
+    for (int i = 0; i < columns; i++)
+    {
+        if (numbers[0, i] > numbers[1, i])
+        {
+            win++;
+            Console.WriteLine($"Win -  {numbers[0, i]} : {numbers[1, i]}");
+        }
+        if (numbers[0, i] < numbers[1, i])
+        {
+            lose++;
+            Console.WriteLine($"Lose -  {numbers[0, i]} : {numbers[1, i]}");
+        }
+        if (numbers[0, i] == numbers[1, i])
+        {
+            lose++;
+            Console.WriteLine($"Draw -  {numbers[0, i]} : {numbers[1, i]}");
+        }
+    }
+    Console.WriteLine($"\n Win \t - {win}");
+    Console.WriteLine($"Lose \t - {lose}");
+    Console.WriteLine($"Draw \t - {draw}");
+
+    int points = win * 3 + draw;
+    System.Console.WriteLine($"Points:  {points}");
+}
+
+void FillArrayCustom(int[,] numbers, int goals = 0, int missed = 1)
+{
+    missed++;
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(goals, missed);
+        }
+    }
+}
+
+
+
+Task46();
+Task48();
+Task49();
+Task51();
+TaskArayGame();
+
+
+
